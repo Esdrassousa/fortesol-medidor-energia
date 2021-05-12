5367b1db370b89a2371ff8640a7dd074e0e49f0d
Esdras S S <esdrassousasoares@hotmail.com>
2021-05-12 12:06:47 -0300

    grafico de potencia

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,14 +7,16 @@ import logoFortesol from '../../imagens/logo.png';
 
 export default function Home(){
     return(
-        <div>
+        <div >
             <div class='logo'>
             <img src={logoFortesol} alt='logo'/>
             </div>
             
             <div class='graficos'>
-            <base target="_blank" />
+            
             <iframe class = "corrente" width="450" height="260"  src="https://thingspeak.com/channels/1373277/charts/1?bgcolor=%23ffffff&color=%23d62020&days=1&dynamic=true&type=line"></iframe>
+            <iframe width="450" height="260" src="https://thingspeak.com/channels/1373277/widgets/309831"></iframe>
+
             </div>
         </div>
     )
