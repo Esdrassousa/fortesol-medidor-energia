0bfc5f7e8e93e508a625b300600b9cdd56a988a4
Esdras S S <esdrassousasoares@hotmail.com>
2021-06-23 13:56:35 -0300

    update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React , {useEffect} from 'react'
 import './style.css'
 import ReactDOM from  'react-dom'
-
-
+import { Chart } from "react-google-charts";
+import { useState } from 'react';
+import api from '../../services/api'
 import logoFortesol from '../../imagens/logo.png';
+import {Line} from "react-chartjs-2";
+
 
 export default function Home(){
+
+
+      const [dado ,setDados] = useState([['', 'corrente'], 
+      ['2021-03-20, 09:59:22',5.88],
+      
+     
+      ],
+     );
+
+     async function dados(e){
+        e.preventDefault()
+        await api.get('/').then(response => {
+          var a = response.data
+          setDados(response.data)
+            
+            
+        })
+       
+
+    }
+    /* var b = [['', 'corrente']]  */
+    useEffect(async  () => {
+      await api.get('/').then(response => {
+
+        /* var a = response.data
+        b.push([a[0], a[1]]) */
+        setDados(response.data)
+        /* setDados([['', 'corrente'], [a[0], a[1]]]) */
+        //console.log("o valor de a é: " , b)
+        
+        
+    })
+      },[dado])
+      console.log("o valor é: " , dado)
+      
+      
     return(
         <div >
             <div class='logo'>
@@ -13,15 +52,24 @@ export default function Home(){
             </div>
             
             <div class='graficos'>
-            <div class = "corrente_div">
-            <iframe class = "corrente" width="450" height="260"  src="https://thingspeak.com/channels/1373277/charts/1?bgcolor=%23ffffff&color=%23d62020&days=1&dynamic=true&type=line"></iframe>
-            </div>
 
-            <div class ="potencia_div">
-            <iframe width="450" height="260" src="https://thingspeak.com/channels/1373277/widgets/309831"></iframe>
+
+           
             </div>
-            </div>
+            
+          <form onSubmit={dados}>
+          <Chart onCha
+            width={'500px'}
+            height={'300px'}
+            chartType="LineChart"
+            data={dado}
+            
+            />
+            
+            
+            
+              <button  type='button' type="submit">click</button>
+          </form>
         </div>
     )
 }
-
