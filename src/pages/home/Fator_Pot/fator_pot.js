6216f01c4d508ccbc05819ed1beb47f6ceed4614
Esdras S S <esdrassousasoares@hotmail.com>
2021-07-28 12:36:30 -0300

    update

--- src/pages/home/Fator_Pot/fator_pot.js
+++ src/pages/home/Fator_Pot/fator_pot.js
@@ -13,3 +13,17 @@ export function fator_potencia(response) {
     return [fator_pot1,fator_pot2,fator_pot3]
 
 }
+
+export function fator_potencia_total(response,Vetor_FatorPotTotal,i){
+    var data_horas_mimuto = response[0]
+    var data_horas_mimuto = data_horas_mimuto[0]
+
+    var fator_pot_total  = response[10]
+    var fator_pot_total  = fator_pot_total[0]
+
+    var vetor_junt_fator_pot_total = [data_horas_mimuto[i], fator_pot_total[i]]
+    console.log(vetor_junt_fator_pot_total)
+    Vetor_FatorPotTotal.push(vetor_junt_fator_pot_total)
+
+    return Vetor_FatorPotTotal
+}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -10,7 +10,7 @@ import logoFortesol from '../../imagens/logo.png';
 import event_botoes from './event_botoes'
 import { hiden_linhas_tensao, hiden_linhas_tensao_pos_for, options_tensao,Hiden_linha1_tensao } from './Tensao/hiden_linhas_tensao'
 import { hiden_linhas_corrente, hiden_linhas_corrente_pos_for, options_corrente,Hiden_linha1_corrente} from './Corrente/hidden_linhas_corrente'
-import {fator_potencia} from './Fator_Pot/fator_pot'
+import {fator_potencia , fator_potencia_total} from './Fator_Pot/fator_pot'
 
 global.tempo_aux_para_enviar_backend = 0;
 global.tempo_para_enviar_backend = 0
@@ -36,6 +36,7 @@ export default function Home() {
 
   const [fpot1, setFpot1] = useState([['Label', 'Value'],['FPA', 0],['FPB', 0],['FPC', 0]])
 
+  const [fpotTotal , setFpotTotal] = useState([['', 'Fator de Potencia Geral'], ['', 0]])
 
   async function atualiza_grafico(e) {
 
@@ -58,20 +59,22 @@ export default function Home() {
 
       var Vetor_tensao = hiden_linhas_tensao(global.esconde_linha1_tensao, global.esconde_linha2_tensao, global.esconde_linha3_tensao)
       var Vetor_corrente = hiden_linhas_corrente(global.esconde_linha1_corrente, global.esconde_linha2_corrente, global.esconde_linha3_corrente)
-      
+      var Vetor_FatorPotTotal = [['', 'Fator_POt'],['', 0]]
       for (var i = 0; i < tamanho; i++) {
 
         var Vetor_Corrente = hiden_linhas_corrente_pos_for(response.data, i, Vetor_corrente, global.esconde_linha1_corrente, global.esconde_linha2_corrente, global.esconde_linha3_corrente)
         var Vetor_tensao = hiden_linhas_tensao_pos_for(response.data, i, Vetor_tensao, global.esconde_linha1_tensao, global.esconde_linha2_tensao, global.esconde_linha3_tensao)
-
+        var Vetor_FatorPotTotal = fator_potencia_total(response.data,Vetor_FatorPotTotal,i)
       }
       var fator_pot1
       var fator_pot2
       var fator_pot3
       [fator_pot1,fator_pot2,fator_pot3] = fator_potencia(response.data)
       setFpot1([['Label', 'Value'],['FPA', fator_pot1],['FPB', fator_pot2],['FPC', fator_pot3]])
+      setFpotTotal(Vetor_FatorPotTotal)
       setCorrente(Vetor_Corrente)
       setTensao(Vetor_tensao)
+
 
     })
 
@@ -122,13 +125,14 @@ async function fun_Hiden_linha1_tensao(indice){
 
       var Vetor_tensao = hiden_linhas_tensao(global.esconde_linha1_tensao, global.esconde_linha2_tensao, global.esconde_linha3_tensao)
       var Vetor_corrente = hiden_linhas_corrente(global.esconde_linha1_corrente, global.esconde_linha2_corrente, global.esconde_linha3_corrente)
+      var Vetor_FatorPotTotal = [['', 'Fator_POt'],['', 0]]
       for (var i = 0; i < tamanho; i++) {
 
         var Vetor_Corrente = hiden_linhas_corrente_pos_for(response.data, i, Vetor_corrente, global.esconde_linha1_corrente, global.esconde_linha2_corrente, global.esconde_linha3_corrente)
 
         var Vetor_tensao = hiden_linhas_tensao_pos_for(response.data, i, Vetor_tensao, global.esconde_linha1_tensao, global.esconde_linha2_tensao, global.esconde_linha3_tensao)
 
-
+        var Vetor_FatorPotTotal = fator_potencia_total(response.data,Vetor_FatorPotTotal,i)
 
       }
       var fator_pot1
@@ -136,6 +140,7 @@ async function fun_Hiden_linha1_tensao(indice){
       var fator_pot3
       [fator_pot1,fator_pot2,fator_pot3] = fator_potencia(response.data)
       setFpot1([['Label', 'Value'],['FPA', fator_pot1],['FPB', fator_pot2],['FPC', fator_pot3]])
+      setFpotTotal(Vetor_FatorPotTotal)
       setCorrente(Vetor_Corrente)
       setTensao(Vetor_tensao)
 
@@ -293,11 +298,41 @@ async function fun_Hiden_linha1_tensao(indice){
 
           >
           </Chart>
+          
           <button id='button_L_L1_tensao' class='blue1' onClick={(e) => fun_Hiden_linha1_tensao(1)}>Tensão_A</button>
           <button id='button_L_L2_tensao' class='cinza' onClick={(e) => fun_Hiden_linha1_tensao(2)} >Tensão_B</button>
           <button id='button_L_L3_tensao' class='ambar' onClick={(e) => fun_Hiden_linha1_tensao(3)} >Tensão_C</button>
           <div class='gauge'>
+          
+          
+          <Chart
+
+          width={'99vw'}
+          height={'50vh'}
+          chartType="Gauge"
+          loader={<div>Loading Chart</div>}
+          options={
+              
+            {
+            title: 'Fator de Potência Total',
+            hAxis: {title: 'data',
+             },
+             vAxis: {title: 'Corrente'},
+             backgroundColor: 'transparent',
+          }}
+          chartType="AreaChart"
+          loader={<div>Loading Chart</div>}
+          //chartType="LineChart"
+          data={fpotTotal}
+          ScrollBar
+          rootProps={{ 'data-testid': '10' }}
+
+          >
+          </Chart>
+          
           <h>Fator de Potencia</h>
+          
+          
           <Chart
             width={'40vw'}
             height={'36vh'}
